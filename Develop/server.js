961006907345bf0f930fb95afea8e629dcfbf381
Eshi44 <e.hameliervolino@gmail.com>
2020-04-02 09:37:16 -0400

    added middleware for express to parse JSON

--- Develop/server.js
+++ Develop/server.js
@@ -4,6 +4,11 @@ const express = require("express");
 //set up express app
 const app = express();
 
+//set a port to run at 3000
 const PORT = process.env.PORT || 3000;
+
+//set up the Express app - middleware to parse JSON data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 
