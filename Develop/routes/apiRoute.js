341d175e7fcc2f6e2a677680e4c82485240f17c6
Eshi44 <e.hameliervolino@gmail.com>
2020-04-02 20:48:57 -0400

    let a variable be req.body, require json file as variable,empties db.json file to an empty array, and made it so new notes that get made are pushed into the req.body at their id

--- Develop/routes/apiRoute.js
+++ Develop/routes/apiRoute.js
@@ -1,33 +1,51 @@
 //require file system
 const fs = require("fs");
 
+//require  the json file
+const json = require("../db/db");
+
 
 
 //create a variable of an array of objects as example
 //dummy data
-var notesExample = [
-    {
-    "title":"Title 1",
-    "text":"text",
-    "id":1
-    },
-    {
-    "title":"Title 2",
-    "text":"text",
-    "id":2
-    }  
-];
+// var notesExample = [
+//     {
+//     "title":"Title 1",
+//     "text":"text",
+//     "id":1
+//     },
+//     {
+//     "title":"Title 2",
+//     "text":"text",
+//     "id":2
+//     }  
+// ];
 
 
-  // notes route
+  // notes route - this file has been exported as a funtion - 
+  //need to require it in the server.js file
   module.exports = function(app) {
 
     //to display notes example
   app.get("/notes", function(req, res) {
-    res.json(notesExample);
+    res.json(json);
   });
 
+  //create a new post
+  app.post("/api/notes", function(req,res) {
 
+    //req.body is an obj containing text parameters 
+    //from the parsed request body -defaults to {}.
+    let inputtedNote = req.body;
+
+    //check and get last id in json file
+    let id = json[json.length - 1]["id"];
+    //let note have one greater id
+    let finalId = id +1;
+    inputtedNote["id"] =finalId;
+    
+    json.push(inputtedNote);
+  });
   
  
 
